71631619fb1708b2b145862e03f28bb57da9bc62
JVBGS <61090053+JVBGS@users.noreply.github.com>
2020-09-29 15:17:28 +0100

    Update script.js

--- html/script.js
+++ html/script.js
@@ -1,20 +1,20 @@
 var socket;
-var usernameInput;
+var usernameInput
 var chatIDInput;
 var messageInput;
 var chatRoom;
 var dingSound;
 var messages = [];
-var delay=true;
+var delay = true;
 
 function onload(){
   socket = io();
-  usernameInput = document.getElementById("IDInput");
-  chatIDInput = document.getElenentById("IDInput");
-  messageInput = document.getElementById("Composed Message");
+  usernameInput = document.getElementById("NameInput");
+  chatIDInput = document.getElementById("IDInput");
+  messageInput = document.getElementById("ComposedMessage");
   chatRoom = document.getElementById("RoomID");
   dingSound = document.getElementById("Ding");
-  
+
   socket.on("join", function(room){
     chatRoom.innerHTML = "Chatroom : " + room;
   })
@@ -30,9 +30,9 @@ function onload(){
       messages.shift();
       messages.push(message);
     }
-    for (i = 0 ; i < messages.length; i++){
-      document.getElementById("Message"+i).innerHTML = messages[i];
-      document.getElementById("Message"+i).stule.color="#303040";
+    for (i = 0; i < messages.length; i++){
+        document.getElementById("Message"+i).innerHTML = messages[i];
+        document.getElementById("Message"+i).style.color = "#303030";
     }
   })
 }
@@ -42,7 +42,7 @@ function Connect(){
 }
 
 function Send(){
-  if (delay&&messageInput.value.replace(/\s/g, "") !=""){
+  if (delay && messageInput.value.replace(/\s/g, "") != ""){
     delay = false;
     setTimeout(delayReset, 1000);
     socket.emit("send", messageInput.value);
