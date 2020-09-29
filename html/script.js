d97d77912ed52a13b415b8c974cb46b2f4cb6926
JVBGS <61090053+JVBGS@users.noreply.github.com>
2020-09-29 09:17:34 +0100

    Update script.js

--- html/script.js
+++ html/script.js
@@ -6,3 +6,18 @@ var chatRoom;
 var dingSound;
 var messages = [];
 var delay=true;
+
+function onload(){
+  socket = io();
+  usernameInput = document.getElementById("IDInput");
+  chatIDInput = document.getElenentById("IDInput");
+  messageInput = document.getElementById("Composed Message");
+  chatRoom = document.getElementById("RoomID");
+  dingSound = document.getElementById("Ding");
+  
+socket.on("join", function(room){
+  chatRoom.innerHTML = "Chatroom : " + room;
+})
+
+socket.on("recieve", function(message){
+  console.log(message);
