2d63f09756cc01907d953472bf56f4c255204666
JVBGS <61090053+JVBGS@users.noreply.github.com>
2020-09-29 14:33:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const path = require("path");
+
+const app = express();
+const httpserver = http.Server(app);
+const io = socketio(httpserver);
+
+const gamedirectory = path.join(_dirname, "html");
+
+app.use(express.static(gamedirectory));
+
+httpserver.listen(3000);
+
+var rooms = [];
+var usernames = [];
+
+io.on("connection",function(socket){
+  
+  socket.on("join", function(room, username){
+    if (username !=""){
+      room[socket.id] = room;
+      username[socket.id] = username;
+      socket.leaveAll();
+      socket.join(room);
+      io.in(room).emit("recieve", "Server : " + username + "has entered the chat.");
+      socket.emit("join", room);
+    }
+  })
+  
+  socket.on("send", function(message){
+    io.in(rooms[socket.id]).emit("recieve", username[socket.id] +" : " + message);
+  })
