330e2ef4a1ce50772921c2e081c42100c9860d50
JVBGS <61090053+JVBGS@users.noreply.github.com>
2020-09-29 09:53:46 +0100

    Update script.js

--- html/script.js
+++ html/script.js
@@ -15,23 +15,23 @@ function onload(){
   chatRoom = document.getElementById("RoomID");
   dingSound = document.getElementById("Ding");
   
-socket.on("join", function(room){
-  chatRoom.innerHTML = "Chatroom : " + room;
-})
+  socket.on("join", function(room){
+    chatRoom.innerHTML = "Chatroom : " + room;
+  })
 
-socket.on("recieve", function(message){
-  console.log(message);
-  if (messages.length < 9){
-    messages.push(message);
-    dingSound.currentTime = 0;
-    dingSound.play();
-  }
-  else{
-    messages.shift();
-    messages.push(message);
-  }
-  for (i = 0 ; i < messages.length; i++){
-    document.getElementById("Message"+i).innerHTML = messages[i];
-    document.getElementById("Message"+i).stule.color="#303040";
-  }
-})
+  socket.on("recieve", function(message){
+    console.log(message);
+    if (messages.length < 9){
+      messages.push(message);
+      dingSound.currentTime = 0;
+      dingSound.play();
+    }
+    else{
+      messages.shift();
+      messages.push(message);
+    }
+    for (i = 0 ; i < messages.length; i++){
+      document.getElementById("Message"+i).innerHTML = messages[i];
+      document.getElementById("Message"+i).stule.color="#303040";
+    }
+  })
